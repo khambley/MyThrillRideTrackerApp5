dd24936e199335d7816a7e10a4d6e4c8ce1d7e84
Katherine Hambley <superdesigngirl@mac.com>
2021-01-13 16:19:21 -0600

    Added Initial Migration, Created db, scaffolded Parks

---
+++ MyThrillRideTrackerApp5/Migrations/20210113203815_InitialCreate.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyThrillRideTrackerApp5.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WebsiteLink = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ParkMapLink = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rides",
+                columns: table => new
+                {
+                    RideId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Height = table.Column<int>(type: "int", nullable: false),
+                    Length = table.Column<int>(type: "int", nullable: false),
+                    TopSpeed = table.Column<int>(type: "int", nullable: false),
+                    GForce = table.Column<int>(type: "int", nullable: false),
+                    RideType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ThrillType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MaterialType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WebsiteLink = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BuildDate = table.Column<DateTime>(type: "date", nullable: false),
+                    Manufacturer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ParkId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rides", x => x.RideId);
+                    table.ForeignKey(
+                        name: "FK_Rides_Parks_ParkId",
+                        column: x => x.ParkId,
+                        principalTable: "Parks",
+                        principalColumn: "ParkId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Visits",
+                columns: table => new
+                {
+                    VisitId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VisitDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ParkId = table.Column<int>(type: "int", nullable: false),
+                    VisitRating = table.Column<int>(type: "int", nullable: false),
+                    VisitComments = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Visits", x => x.VisitId);
+                    table.ForeignKey(
+                        name: "FK_Visits_Parks_ParkId",
+                        column: x => x.ParkId,
+                        principalTable: "Parks",
+                        principalColumn: "ParkId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VisitRides",
+                columns: table => new
+                {
+                    VisitRideId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RideId = table.Column<int>(type: "int", nullable: false),
+                    VisitId = table.Column<int>(type: "int", nullable: false),
+                    VisitRideRating = table.Column<int>(type: "int", nullable: false),
+                    VisitRideComments = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VisitRides", x => x.VisitRideId);
+                    table.ForeignKey(
+                        name: "FK_VisitRides_Rides_RideId",
+                        column: x => x.RideId,
+                        principalTable: "Rides",
+                        principalColumn: "RideId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_VisitRides_Visits_VisitId",
+                        column: x => x.VisitId,
+                        principalTable: "Visits",
+                        principalColumn: "VisitId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ImageFileNames",
+                columns: table => new
+                {
+                    ImageFileNameId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FileName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ParkId = table.Column<int>(type: "int", nullable: true),
+                    RideId = table.Column<int>(type: "int", nullable: true),
+                    VisitId = table.Column<int>(type: "int", nullable: true),
+                    VisitRideId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ImageFileNames", x => x.ImageFileNameId);
+                    table.ForeignKey(
+                        name: "FK_ImageFileNames_Parks_ParkId",
+                        column: x => x.ParkId,
+                        principalTable: "Parks",
+                        principalColumn: "ParkId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ImageFileNames_Rides_RideId",
+                        column: x => x.RideId,
+                        principalTable: "Rides",
+                        principalColumn: "RideId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ImageFileNames_VisitRides_VisitRideId",
+                        column: x => x.VisitRideId,
+                        principalTable: "VisitRides",
+                        principalColumn: "VisitRideId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ImageFileNames_Visits_VisitId",
+                        column: x => x.VisitId,
+                        principalTable: "Visits",
+                        principalColumn: "VisitId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImageFileNames_ParkId",
+                table: "ImageFileNames",
+                column: "ParkId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImageFileNames_RideId",
+                table: "ImageFileNames",
+                column: "RideId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImageFileNames_VisitId",
+                table: "ImageFileNames",
+                column: "VisitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImageFileNames_VisitRideId",
+                table: "ImageFileNames",
+                column: "VisitRideId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rides_ParkId",
+                table: "Rides",
+                column: "ParkId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VisitRides_RideId",
+                table: "VisitRides",
+                column: "RideId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VisitRides_VisitId",
+                table: "VisitRides",
+                column: "VisitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Visits_ParkId",
+                table: "Visits",
+                column: "ParkId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ImageFileNames");
+
+            migrationBuilder.DropTable(
+                name: "VisitRides");
+
+            migrationBuilder.DropTable(
+                name: "Rides");
+
+            migrationBuilder.DropTable(
+                name: "Visits");
+
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
---
+++ MyThrillRideTrackerApp5/Models/ThrillRideTrackerDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyThrillRideTrackerApp5.Models
+{
+	public class ThrillRideTrackerDbContext : DbContext
+	{
+		public ThrillRideTrackerDbContext (DbContextOptions<ThrillRideTrackerDbContext> options)
+			: base(options) { }
+
+		public DbSet<Park> Parks { get; set; }
+		public DbSet<Ride> Rides { get; set; }
+		public DbSet<Visit> Visits { get; set; }
+		public DbSet<VisitRide> VisitRides { get; set; }
+		public DbSet<ImageFileName> ImageFileNames { get; set; }
+	}
+}
